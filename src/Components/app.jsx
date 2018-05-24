67b7cd4c11db42315c14277f0df1fd24e5721486
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-05-24 11:36:57 -0700

    app now rendering dynamic passed-in data

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -3,25 +3,21 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      currentSearch = '';
+      currentSearch : ''
     }
   }
 
   render() {
+
     return (
       <div id="movie-list-container">
       	<div id="title-bar">
         	<h3> MovieList </h3>
         </div>
         <div id="body-container">
-          {this.props.data.forEach(movie => {
-            <MovieListEntry mov={movie}/>
+          {this.props.data.map(movie => {
+            return <MovieListEntry mov={movie}/>
           })}
-          <div id="movie-list-entry"> {props.data[0].title} </div>
-          <div id="movie-list-entry"> {props.data[1].title} </div>
-          <div id="movie-list-entry"> {props.data[2].title} </div>
-          <div id="movie-list-entry"> {props.data[3].title} </div>
-          <div id="movie-list-entry"> {props.data[4].title} </div>
         </div>
       </div>
     )
--- src/Components/movieListEntry.jsx
+++ src/Components/movieListEntry.jsx
@@ -1,5 +1,5 @@
 var MovieListEntry = (props) => (
-  <div id="movie-list-entry"> {props.mov} </div>
+  <div id="movie-list-entry"> {props.mov.title} </div>
 );
 
 window.MovieListEntry = MovieListEntry;
